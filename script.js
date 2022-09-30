a317d59cc15ea14c5c43ce6042f757eb3a7cd3c9
M_Coder <m.mamatkulov95@gmail.com>
2022-09-30 06:25:53 +0500

    UI improvments and js basics

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
+let myLeads = [];
 const inputBtn = document.querySelector(".input-btn")
 const inputEl = document.querySelector(".input-el")
 const ulEL = document.querySelector(".ul-el")
-let myLeads = ["Abdulloh", "Sanjoar", "Rano"];
+
 
 
 inputBtn.addEventListener('click', function click(){
-    myLeads.push(inputBtn.value);
-    console.log(myLeads)
+    myLeads.push(inputEl.value);
+    inputEl.value = ""
+    renderLeads();
 })
 
-for(let i = 0; i<myLeads.length; i++){
 
-    ulEL.textContent += myLeads[i]
-
+function renderLeads(){
+    let listItems = ""
+    for(let i = 0; i<myLeads.length; i++){
+        // listItems += "<li> <a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i]  + "</a> </li>"
+        listItems += `
+        <li>
+            <a target='_blank' href="${myLeads[i]}">
+                ${myLeads[i]}
+            </a>
+        </li>`
+    }
+    ulEL.innerHTML = listItems
 }
-
